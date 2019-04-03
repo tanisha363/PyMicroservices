8d23934c383444cbe8860e930192a56b3eb347d1
nagenn <44678680+nagenn@users.noreply.github.com>
2019-04-03 14:46:45 +0530

    Update test_addbook.py

--- Source/test_addbook.py
+++ Source/test_addbook.py
@@ -1,10 +1,11 @@
 import json
+import unittest
 from book import Book
 #from delhander import DelHandler
 from addhandler import AddHandler
 
 books = Book()
-
-def test_add():
-    result = books.add_book('Book1', 'Author1')
-    assert result
+class addTest(unittest.TestCase):
+    def test_add(self):
+        result = books.add_book('Book1', 'Author1')
+        assert result
