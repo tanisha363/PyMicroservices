5c2922fc91b79a06d4026defb478b1a94a78e852
nagenn <44678680+nagenn@users.noreply.github.com>
2019-11-12 17:15:50 +0530

    Update pre-build-config.bat

--- Source/pre-build-config.bat
+++ Source/pre-build-config.bat
@@ -1,2 +1,3 @@
 SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
+git pull
 gcloud auth activate-service-account jenkins@handle-books.iam.gserviceaccount.com --key-file=c:\Users\nvmur\Downloads\handle-books-2a419c24f2ac.json --project=handle-books
