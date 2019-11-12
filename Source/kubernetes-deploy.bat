9c06029ced37683a662c4faa9853ec992ee344de
nagenn <44678680+nagenn@users.noreply.github.com>
2019-11-12 17:34:33 +0530

    Update kubernetes-deploy.bat

--- Source/kubernetes-deploy.bat
+++ Source/kubernetes-deploy.bat
@@ -1,5 +1,4 @@
 SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
-SET BUILD_NUMBER=53
 SET IMAGE_NAME=gcr.io/handle-books/gpymicro-image:%BUILD_NUMBER%
 cd c:\skillvelocity\courses\NU\Cloud\devops\PyMicroservices\Source
 kubectl set image deployment/py-gmicro py-gmicro=%IMAGE_NAME%
