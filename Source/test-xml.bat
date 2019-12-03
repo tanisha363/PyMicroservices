0cf273a10ef4c05e6694d32ef466171d19ba6d54
nagenn <44678680+nagenn@users.noreply.github.com>
2019-12-03 23:05:44 +0530

    Update test-xml.bat

--- Source/test-xml.bat
+++ Source/test-xml.bat
@@ -1,11 +1,11 @@
 SET PATH=%PATH%;c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32;c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\Scripts
-del C:\skillvelocity\courses\devops\PyMicroservices\Source\python_tests_xml\*.*
-del C:\skillvelocity\courses\devops\PyMicroservices\Source\coverage.xml
-cd c:\skillvelocity\courses\devops\PyMicroservices\Source
+del C:\skillvelocity\courses\NU\Cloud\devops\Pymicroservices\python_tests_xml\*.*
+del C:\skillvelocity\courses\NU\Cloud\devops\Pymicroservices\Source\coverage.xml
+cd C:\skillvelocity\courses\NU\Cloud\devops\Pymicroservices\Source
 REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python runtests.py
 REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python -m coverage run -m pytest && coverage xml -o coverage.xml
 REM c:\Users\nvmur\Appdata\Local\Programs\Python\Python37-32\python -m coverage html -d coverage
 python runtests.py
 python -m coverage run -m pytest && coverage xml -o coverage.xml
-copy C:\skillvelocity\courses\devops\PyMicroservices\Source\python_tests_xml\*.* C:\skillvelocity\courses\devops\PyMicroservices\python_tests_xml
-copy C:\skillvelocity\courses\devops\PyMicroservices\Source\coverage.xml C:\skillvelocity\courses\devops\PyMicroservices
+copy C:\skillvelocity\courses\NU\Cloud\devops\Pymicroservices\Source\python_tests_xml\*.* C:\skillvelocity\courses\NU\Cloud\devops\Pymicroservices\python_tests_xml
+copy C:\skillvelocity\courses\NU\Cloud\devops\Pymicroservices\Source\coverage.xml C:\skillvelocity\courses\NU\Cloud\devops\Pymicroservices
