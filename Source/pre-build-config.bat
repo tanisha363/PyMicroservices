5ecfcaf378137272c6cf49d8cc1f08ce00a27f8c
nagenn <44678680+nagenn@users.noreply.github.com>
2019-12-04 11:10:56 +0530

    Update pre-build-config.bat

--- Source/pre-build-config.bat
+++ Source/pre-build-config.bat
@@ -1,3 +1,3 @@
 SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
-git pull
+git pull "https://github.com/nagenn/PyMicroservices"
 gcloud auth activate-service-account jenkins@handle-books.iam.gserviceaccount.com --key-file=c:\Users\nvmur\Downloads\handle-books-2a419c24f2ac.json --project=handle-books
