c19746922be20ff2077f1b6b91273e28f8dff613
nagenn <44678680+nagenn@users.noreply.github.com>
2020-11-12 16:53:52 +0530

    Update kubernetes-deploy.bat

--- Source/kubernetes-deploy.bat
+++ Source/kubernetes-deploy.bat
@@ -1,4 +1,4 @@
 SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
 SET IMAGE_NAME=gcr.io/books-nu/microbooks-image:%BUILD_NUMBER%
 cd c:\skillvelocity\courses\NU\Cloud\devops\PyMicroservices\Source
-kubectl set image deployment/nu-deploy1 nu-deploy1=%IMAGE_NAME%
+kubectl set image deployment nu-deploy1 nu-deploy1=%IMAGE_NAME%
