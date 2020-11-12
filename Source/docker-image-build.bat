8779038e9ef14fd3d67e1d748db94285f6be4da0
nagenn <44678680+nagenn@users.noreply.github.com>
2020-11-12 16:45:01 +0530

    Update docker-image-build.bat

--- Source/docker-image-build.bat
+++ Source/docker-image-build.bat
@@ -1,4 +1,4 @@
 SET PATH=%PATH%;C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin
-SET IMAGE_NAME=gcr.io/handle-books/gpymicro-image:%BUILD_NUMBER%
+SET IMAGE_NAME=gcr.io/books-nu/microbooks-image:%BUILD_NUMBER%
 cd c:\skillvelocity\courses\NU\Cloud\devops\PyMicroservices\Source
 gcloud builds submit --tag %IMAGE_NAME% .
